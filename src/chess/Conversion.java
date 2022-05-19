aa46fcdb64b65ebdf1d51fda7b27afefd24ce836
Sam Jones <sfjones0@gmail.com>
2022-05-19 18:54:35 -0400

    Documentation

--- src/chess/Conversion.java
+++ src/chess/Conversion.java
@@ -1,11 +1,24 @@
 package chess;
 import java.util.HashMap;
 
+/**
+ * The <code>Conversion</code> class contains two hash maps. The first is for the type of a given piece.
+ * The key is a <code>Character</code> (as specified in the project description) and the value is the
+ * piece type as a <code>String</code>. The second hash map retrieves information about the column a piece
+ * is in. An <code>Integer</code> - the column array index - is the key and a <code>Character</code> -
+ * the corresponding official chess column - is the value.
+ * 
+ * @author Sam Jones
+ */
 public class Conversion {
 
     private static HashMap<Character, String> piece_type;
     private static HashMap<Integer, Character> piece_col;
 
+
+    /**
+     * Initializes the two hash maps if they are null.
+     */
     private static void init(){
         piece_type = new HashMap<Character, String>();
         piece_type.put('p', "Pawn");
@@ -26,6 +39,19 @@ public class Conversion {
         piece_col.put(7, 'h');
     }
 
+
+    /**
+     * Initializes the two hash maps in the {@code Conversion} class if they are null. Then, takes the
+     * inputted piece type and returns the full {@code String} version of it. This method uses the
+     * piece_type hash map.
+     * <p>
+     * This is a static method so that an instance of the {@code Conversion} class does not need to be
+     * created in order to call this method.
+     * 
+     * @param type The type of the piece as specified in the project description
+     * @return The corresponding {@code String} version of the piece type, retrieved from the
+     * piece_type hash map
+     */
     public static String retrieve_type(char type){
 
         if (piece_type == null && piece_col == null){
@@ -35,6 +61,19 @@ public class Conversion {
         return piece_type.get(type);
     }
 
+
+    /**
+     * Initializes the two hash maps in the {@code Conversion} class if they are null. Then, takes the
+     * inputted piece column and returns the {@code char} version of it. This method uses the
+     * piece_col hash map.
+     * <p>
+     * This is a static method so that an instance of the {@code Conversion} class does not need to be
+     * created in order to call this method.
+     * 
+     * @param col The current column the piece is in (integer array index)
+     * @return The corresponding {@code char} version of the current column, retrieved from the
+     * piece_col hash map
+     */
     public static char retrieve_col(int col){
 
         if (piece_type == null && piece_col == null){
