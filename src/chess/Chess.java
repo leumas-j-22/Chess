3a7ae7aa992ea1874fea17e16fa159a8cef1375a
Sam Jones <sfjones0@gmail.com>
2022-05-20 17:05:01 -0400

    Added resign method and also moved some code to Setup.java

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -13,8 +13,10 @@ import java.io.IOException;
  */
 public class Chess {
 
-    private static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
     private static Piece[][] board = new Piece[8][8];
+    private static int moveNumber = 0;
+    private static boolean play = true;
 
     /**
      * Checks if the row and column values for a starting square and ending square are out of bounds.
@@ -41,73 +43,6 @@ public class Chess {
         }
 
         return false;
-    }
-
-
-    /**
-     * Sets the board at the beginning of the game.
-     */
-    private static void setBoard(){
-
-        // Pawns
-        for (int col = 0; col < 8; col++){
-            Piece bPawn = new Pawn(board, 1, col, 'b', 'p');
-            Piece wPawn = new Pawn(board, 6, col, 'w', 'p');
-            bPawn.addPiece();
-            wPawn.addPiece();
-        }
-
-        
-        // Knights
-        Piece bKnight1 = new Knight(board, 0, 1, 'b', 'N');
-        Piece bKnight2 = new Knight(board, 0, 6, 'b', 'N');
-        Piece wKnight1 = new Knight(board, 7, 1, 'w', 'N');
-        Piece wKnight2 = new Knight(board, 7, 6, 'w', 'N');
-
-        bKnight1.addPiece();
-        bKnight2.addPiece();
-        wKnight1.addPiece();
-        wKnight2.addPiece();
-
-
-        // Bishops
-        Piece bBishop1 = new Bishop(board, 0, 2, 'b', 'B');
-        Piece bBishop2 = new Bishop(board, 0, 5, 'b', 'B');
-        Piece wBishop1 = new Bishop(board, 7, 2, 'w', 'B');
-        Piece wBishop2 = new Bishop(board, 7, 5, 'w', 'B');
-
-        bBishop1.addPiece();
-        bBishop2.addPiece();
-        wBishop1.addPiece();
-        wBishop2.addPiece();
-
-
-        // Rooks
-        Piece bRook1 = new Rook(board, 0, 0, 'b', 'R');
-        Piece bRook2 = new Rook(board, 0, 7, 'b', 'R');
-        Piece wRook1 = new Rook(board, 7, 0, 'w', 'R');
-        Piece wRook2 = new Rook(board, 7, 7, 'w', 'R');
-
-        bRook1.addPiece();
-        bRook2.addPiece();
-        wRook1.addPiece();
-        wRook2.addPiece();
-
-
-        // Queens
-        Piece bQueen = new Queen(board, 0, 3, 'b', 'Q');
-        Piece wQueen = new Queen(board, 7, 3, 'w', 'Q');
-
-        bQueen.addPiece();
-        wQueen.addPiece();
-
-
-        // Kings
-        Piece bKing = new King(board, 0, 4, 'b', 'K');
-        Piece wKing = new King(board, 7, 4, 'w', 'K');
-
-        bKing.addPiece();
-        wKing.addPiece();
     }
 
 
@@ -143,18 +78,24 @@ public class Chess {
     }
 
 
-    private static boolean init(){
+    private static void resign(BufferedWriter bw){
+        try{
+            bw.write("resign");
+            bw.newLine();
 
-        String continuation;
-        System.out.print("Continue with the saved game or start a new game? Enter 'y' to continue, 'n' to restart: ");
+            if (moveNumber % 2 == 0){
+                System.out.println("Black wins");
+                bw.write("Black wins");
+            }
+            else {
+                System.out.println("White wins");
+                bw.write("White wins");
+            }
 
-        while (true){
-            continuation = scanner.nextLine();
-            continuation = continuation.trim();
-
-            if (continuation.equals("y")) return true;
-            else if (continuation.equals("n")) return false;
-            else System.out.print("Invalid input. Please enter 'y' to continue where you left off, or 'n' to start a new game: ");
+            play = false;
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
     }
 
@@ -167,16 +108,20 @@ public class Chess {
     public static void main(String[] args){
 
         // TODO - add in case if there is no existing game that was paused
-        boolean continuation = init();
+        boolean continuation = Setup.init();
         
         try {
-            FileWriter fw = new FileWriter("data/move-list.txt", true);
+            FileWriter fw;
+
+            // Continuing with a paused game - appending to the list of moves
+            if (continuation) fw = new FileWriter("data/move-list.txt", true);
+            // Starting a new game - truncating any existing contents in the list of moves
+            else fw  = new FileWriter("data/move-list.txt", false);
+
             BufferedWriter bw = new BufferedWriter(fw);
-            int moveNumber = 0;
-            boolean play = true;
             String move;
 
-            setBoard();
+            Setup.setBoard(board);
             printBoard();
     
             while (play){
@@ -185,9 +130,11 @@ public class Chess {
                 move = move.trim();
     
                 if (move.equals("resign")){
-    
+                    resign(bw);
                 }
-    
+
+
+                moveNumber++;
             }
 
             bw.close();
